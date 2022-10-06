ef8388de67ea103b01771e4f747586af21d59f38
pi8074 <70912821+pi8074@users.noreply.github.com>
2022-10-06 22:02:10 +0200

    aahhhh

---
+++ async-await-callbacks/main.js
@@ -0,0 +1,10 @@
+let stocks={
+    Fruits: ["bannana","grapes","apple","strawberry"],
+    Liquids:["whater","ice"],
+    Holder:["cone","cup","stick"],
+    Toppings:["chockolate","penuts"]
+}
+
+
+
+
---
+++ async-await-callbacks/random.js
@@ -0,0 +1,65 @@
+// function one(call_two){
+//     console.log("step1_done calling step 2")
+//     call_two()
+// }
+// function two(){
+//     console.log("step2")
+   
+// }
+// one(two)
+
+
+
+
+// step1_done call step 2
+//  step2
+
+
+
+// let order = (call_production)=>{
+//     console.log("order placed") 
+//     call_production()}
+// let production = ()=>{console.log("order receved")}
+// order(production)
+
+
+
+// order placed
+// order receved
+
+
+
+
+
+//// callback hell nie wiem czemu to pisałem 
+
+// let order = (Fruit_name, call_production)=>{
+//     setTimeout(()=>{
+//         console.log(`${stocks.Fruits[Fruit_name]} was selected`)
+//         call_production()
+//     },2000)
+    
+// }
+// let production = ()=>{
+//     setTimeout(()=>{
+//         console.log("production has started")
+
+//         setTimeout(()=>{
+//             console.log("the fruit has been choped")
+//             setTimeout(()=>{
+//                 console.log(`${stocks.Liquids[1]} and ${stocks.Liquids[0]} was added`)
+//                 setTimeout(()=>{
+//                     console.log("the mashene was started")
+//                     setTimeout(()=>{
+//                         console.log(`in a ${stocks.Holder[0]}`)
+//                         setTimeout(()=>{
+//                             console.log(` ${stocks.Toppings[0]} was placed`)
+                            
+//                         },3000)
+//                     },2000)
+//                 },1000)
+//             },1000)
+//         },2000)
+//     },0000)
+// }
+// order(0,production)
